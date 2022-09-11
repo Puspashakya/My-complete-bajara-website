2578bdcfa91b5b9ac5c59ca813ec3d9c6caea62e
puspa shakya <84306848+Puspashakya@users.noreply.github.com>
2022-09-11 13:40:37 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,121 @@
+<?php
+include "includes/head.php"
+?>
+
+<body>
+
+  <div class="site-wrap">
+    <?php
+    include "includes/header.php"
+    ?>
+    <div class="site-blocks-cover" style="background-image: url('images/back.jpg');">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7 mx-auto order-lg-2 align-self-center">
+            <div class="site-block-cover-content text-center">
+
+              <h1>Bajra Store </h1>
+              <p>
+                <a href="store.php" class="btn btn-primary px-5 py-3">Shop Now</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+   
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row">
+          <div class="title-section text-center col-12">
+            <h2 class="text-uppercase">Products You Might Like</h2>
+          </div>
+        </div>
+
+        <div class="row">
+          <?php
+          $data = all_products();
+          $num = sizeof($data);
+          for ($i = 0; $i < $num; $i++) {
+          ?>
+            <div class="col-sm-6 col-lg-4 text-center item mb-4">
+              <a href="product.php?product_id=<?php echo $data[$i]['item_id'] ?>"> <img class="rounded mx-auto d-block" style="width:80% ; height:80% ;" src="images/<?php echo $data[$i]['item_image'] ?>" alt="Image"></a>
+              <?php if (strlen($data[$i]['item_title']) <= 20) { ?>
+                <h3 class="text-dark"><a href="product.php?product_id=<?php echo $data[$i]['item_id'] ?>"><?php echo $data[$i]['item_title'] ?></a></h3>
+              <?php
+              } else {
+              ?>
+                <h3 class="text-dark"><a href="product.php?product_id=<?php echo $data[$i]['item_id'] ?>"><?php echo substr($data[$i]['item_title'], 0, 20) . "..." ?></a></h3>
+              <?php
+              }
+              ?>
+              <p class="price">Rs. <?php echo $data[$i]['item_price'] ?></p>
+            </div>
+          <?php
+            if ($i == 5) {
+              break;
+            }
+          }
+          ?>
+        </div>
+        <div class="row mt-5">
+          <div class="col-12 text-center">
+            <a href="store.php" class="btn btn-primary px-4 py-3">View All Products</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="site-section bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="title-section text-center col-12">
+            <h2 class="text-uppercase">New Products</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 block-3 products-wrap">
+            <div class="nonloop-block-3 owl-carousel">
+              <?php
+              $data = all_products_reverse();
+
+              $num = sizeof($data);
+              for ($i = 0; $i < $num; $i++) {
+              ?>
+                <!--  -->
+                <div class="  text-center item mb-4">
+                  <a href="product.php?product_id=<?php echo $data[$i]['item_id'] ?>"> <img class="rounded mx-auto d-block" style="width:270px ; height:300px ;" src="images/<?php echo $data[$i]['item_image'] ?>" alt="Image"></a>
+
+                  <h3 class="text-dark"><a href="product.php?product_id=<?php echo $data[$i]['item_id'] ?>">Recently added</a></h3>
+
+                  <p class="price">Rs. <?php echo $data[$i]['item_price'] ?></p>
+                </div>
+                <!--  -->
+              <?php
+                if ($i == 5) {
+                  break;
+                }
+              }
+              ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  
+
+
+    <?php
+    include "includes/footer.php"
+    ?>
+  </div>
+
+
+
+</body>
+
+</html>
---
+++ product.php
@@ -0,0 +1,119 @@
+<?php
+include "includes/head.php"
+?>
+
+<body>
+
+  <div class="site-wrap">
+    <?php
+    include "includes/header.php";
+    $data = get_item();
+    add_cart($_SESSION['item_id']);
+    ?>
+
+    <div class="bg-light py-3">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 mb-0"><a href="index.php">Home</a> <span class="mx-2 mb-0">/</span> <a href="store.php">Store</a> <span class="mx-2 mb-0">/</span> <strong class="text-black"><?php echo $data[0]['item_title'] ?></strong></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5 mr-auto">
+            <div class="border text-center">
+              <img src="images/<?php echo $data[0]['item_image'] ?>" alt="Image" class="img-fluid p-5">
+            </div>
+          </div>
+          <div class="col-md-6">
+            <h2 class="text-black"><?php echo $data[0]['item_title'] ?></h2>
+            <p><?php echo $data[0]['item_details'] ?></p>
+
+
+            <p><strong class="text-primary h4">Rs. <?php echo $data[0]['item_price'] ?></strong></p>
+            <?php
+            if ($data[0]['item_quantity'] > 0) {
+            ?>
+              <h6 style="color: rgb(58, 211, 58);">In Stock</h6>
+              <form action="product.php" method="GET">
+                <div class="mb-5">
+                  <div class="input-group mb-3" style="max-width: 220px;">
+
+                    <input type="number" min="1" max="<?php echo $data[0]['item_quantity'] ?>" name="quantity" class="form-control text-center" value="1" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1">
+
+                  </div>
+                  <br>
+                  <button name="cart" type="submit" class="btn btn-primary btn-lg">Add To Cart</button>
+              </form>
+            <?php
+            } else {
+            ?>
+              <small style="color: red;">Out Of Stock</small>
+              <form action="product.php" method="GET">
+                <div class="mb-5">
+                  <div class="input-group mb-3" style="max-width: 220px;">
+
+                    <input disabled type="number" name="quantity" class="form-control text-center" value="0" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1">
+
+                  </div>
+                  <br>
+                  <p style="color: black;"><a disabled type="submit" value="" class="buy-now btn btn-sm height-auto px-4 py-3 btn-primary">Add To Cart</a></p>
+
+              </form>
+            <?php
+            }
+            ?>
+
+
+          </div>
+
+          <div class="mt-5">
+          
+            <div class="tab-content" id="pills-tabContent">
+              <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+         
+                </p>
+              </div>
+              <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+
+                <table class="table custom-table">
+
+                  <tbody>
+                    <tr>
+                      <td>BRAND NAME</td>
+                      <td class="bg-light"><?php echo $data[0]['item_brand'] ?></td>
+                    </tr>
+                    <tr>
+                      <td>CATEGORY</td>
+                      <td class="bg-light"><?php echo $data[0]['item_cat'] ?></td>
+                    </tr>
+                    <tr>
+                      <td>DELIVERY FEES</td>
+                      <?php if ($data[0]['item_price'] >= 200) {
+                        echo "<td class='bg-light'>FREE</td>";
+                      } else {
+                        echo "<td class='bg-light'>Rs. 40</td>";
+                      }
+                      ?>
+                    </tr>
+                </table>
+
+              </div>
+
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php
+  include "includes/footer.php"
+  ?>
+  </div>
+</body>
+
+</html>
